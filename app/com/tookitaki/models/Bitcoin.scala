cf72fbd77d9c74cbde00ab46b84b06f8b786a369
Sumit Pawar <sumitppawar@gmail.com>
2018-08-18 20:54:22 +0530

    Cleaning code

--- app/com/tookitaki/models/Bitcoin.scala
+++ app/com/tookitaki/models/Bitcoin.scala
@@ -1,15 +1,11 @@
 package com.tookitaki.models
 
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
 import play.api.libs.json._
 
 case class BitcoinPriceHistory(base: String, currency: String, prices: Seq[Price])
 case class Price(price: String, time: String)
 
 object BitcoinPriceHistory {
-  LocalDate.parse("2013-01-08T00:00:00Z", DateTimeFormatter.ISO_DATE_TIME)
   implicit val priceJsonFormat = Json.format[Price]
   implicit  val bitcoinPriceHistoryJsonFormat = Json.format[BitcoinPriceHistory]
 
